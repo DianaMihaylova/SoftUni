772b6bbf2efcba952acb6136887a845050b69703
Didi <31138364+DianaMihaylova@users.noreply.github.com>
2017-09-15 03:33:48 +0300

    Coding 101 Exam - 24 April 2016

---
+++ Homeworks/Coding 101 Exam - 24 April 2016/Operations.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace _03.Operations
+{
+    class Operations
+    {
+        static void Main()
+        {
+            var nOne = int.Parse(Console.ReadLine());
+            var nTwo = int.Parse(Console.ReadLine());
+            string symbol = Console.ReadLine();
+            var plusResult = nOne + nTwo;
+            var minResult = nOne - nTwo;
+            var multResult = nOne * nTwo;
+
+            if (nTwo == 0)
+            {
+                Console.WriteLine("Cannot divide {0} by zero", nOne);
+            }
+            else
+            {
+                switch (symbol)
+                {
+                    case "+":
+                        Console.WriteLine("{0} {1} {2} = {3} - {4}", nOne, symbol, nTwo, plusResult, plusResult % 2 == 0 ? "even" : "odd");
+                        break;
+                    case "-":
+                        Console.WriteLine("{0} {1} {2} = {3} - {4}", nOne, symbol, nTwo, minResult, minResult % 2 == 0 ? "even" : "odd");
+                        break;
+                    case "*":
+                        Console.WriteLine("{0} {1} {2} = {3} - {4}", nOne, symbol, nTwo, multResult, multResult % 2 == 0 ? "even" : "odd");
+                        break;
+                    case "/":
+                        Console.WriteLine("{0} {1} {2} = {3:f2}", nOne, symbol, nTwo, (double)nOne / nTwo);
+                        break;
+                    case "%":
+                        Console.WriteLine("{0} {1} {2} = {3}", nOne, symbol, nTwo, nOne % nTwo);
+                        break;
+                    default:
+                        break;
+                }
+            }
+        }
+    }
+}
---
+++ Homeworks/Coding 101 Exam - 24 April 2016/SmartLilly.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace _04.SmartLilly
+{
+    class SmartLilly
+    {
+        static void Main()
+        {
+            var ageLilly = int.Parse(Console.ReadLine());
+            var priceLaundry = double.Parse(Console.ReadLine());
+            var pricePerToy = int.Parse(Console.ReadLine());
+            var bonus = 10;
+            var sumMoney = 0;
+            var sumToys = 0;
+
+            for (int i = 1; i <= ageLilly; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    sumMoney += bonus;
+                    sumMoney -= 1;
+                    bonus += 10;
+                }
+                else
+                {
+                    sumToys++;
+                }
+            }
+            var saveMoney = sumMoney;
+            var sellToys = sumToys * pricePerToy;
+            var allMoney = saveMoney + sellToys;
+            if (allMoney >= priceLaundry)
+            {
+                Console.WriteLine("Yes! {0:f2}", allMoney - priceLaundry);
+            }
+            else
+            {
+                Console.WriteLine("No! {0:f2}", priceLaundry - allMoney);
+            }
+        }
+    }
+}
