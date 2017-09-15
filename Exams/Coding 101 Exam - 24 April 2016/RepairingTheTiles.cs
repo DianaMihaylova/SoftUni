fe15b465b182f0abe41bd63f5de350e5d5026ab1
Didi <31138364+DianaMihaylova@users.noreply.github.com>
2017-09-15 03:36:14 +0300

    Coding 101 Exam - 24 April 2016

---
+++ Exams/Coding 101 Exam - 24 April 2016/RepairingTheTiles.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace _01.RepairingTheTiles
+{
+    class RepairingTheTiles
+    {
+        static void Main()
+        {
+            var hLand = int.Parse(Console.ReadLine());
+            var wTiles = double.Parse(Console.ReadLine());
+            var hTiles = double.Parse(Console.ReadLine());
+            var wBench = int.Parse(Console.ReadLine());
+            var hBench = int.Parse(Console.ReadLine());
+            var areaLand = hLand * hLand;
+            var areaBench = wBench * hBench;
+            var areaLandTiles = areaLand - areaBench;
+            var areaTiles = wTiles * hTiles;
+            var allAreaTiles = (double)areaLandTiles / areaTiles;
+            var time = allAreaTiles * 0.2;
+            Console.WriteLine(allAreaTiles);
+            Console.WriteLine(time);
+        }
+    }
+}
